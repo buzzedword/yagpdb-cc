c9d31e651b7545f539a00bd55f7543c3ede5cba0
Danny Garcia <dev.danny.garcia@gmail.com>
2021-01-23 01:31:13 +0000

    Add better whitespace management

--- andbox/getcodeofconduct.lua
+++ andbox/getcodeofconduct.lua
@@ -1,11 +1,9 @@
 {{ $args := parseArgs 1 "" (carg "string" "Look up rule") }}
 {{ $rules:= split ($args.Get 0) "," }}
-{{ $rulebook := cslice "" }}
+
 {{ range $index, $rule := $rules }}
-{{$stored_rule := dbGet  0 $rule}}
-{{if $stored_rule}}
-{{$rulebook.Append $stored_rule.Value}}
+    {{$stored_rule := dbGet  0 $rule}}
+    {{- if $stored_rule -}}
+        {{print $stored_rule.Value}}
+    {{- end -}}
 {{end}}
-{{end}}
-{{$violations := $rulebook.StringSlice}}
-{{joinStr "\r" $violations}}
